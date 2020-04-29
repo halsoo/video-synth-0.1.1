e8cb845b74ebe676391c7b78d0b4be4cbbc89321
UKGU <UKGU@UKGUs-MacBookPro.local>
2020-04-29 16:37:41 +0900

    Updates

---
+++ precache-manifest.e11f4ce9dec82641d0d98c5ac16dcd1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc449234b0041a8251843ac89b0d6dfd",
+    "url": "/halsoo.github.io/video-synth-0.1.1/index.html"
+  },
+  {
+    "revision": "8d591c42205212128b36",
+    "url": "/halsoo.github.io/video-synth-0.1.1/static/css/main.0971ebe9.chunk.css"
+  },
+  {
+    "revision": "ca983069ab589becf244",
+    "url": "/halsoo.github.io/video-synth-0.1.1/static/js/2.d4b0895e.chunk.js"
+  },
+  {
+    "revision": "a8a8a80bfe218b417766dbf9de8895fd",
+    "url": "/halsoo.github.io/video-synth-0.1.1/static/js/2.d4b0895e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d591c42205212128b36",
+    "url": "/halsoo.github.io/video-synth-0.1.1/static/js/main.4361c862.chunk.js"
+  },
+  {
+    "revision": "2fa77f020124e77db451",
+    "url": "/halsoo.github.io/video-synth-0.1.1/static/js/runtime-main.5546b8d1.js"
+  }
+]);
